576b2b75d1fe1481203527a0cb89d60e155529a9
sushma0918 <sushmamanapuram18@gmail.com>
2025-12-12 14:23:26 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,8 @@
+name=input("enter name")
+print(name,type(name))
+roll=int(input("enter number"))
+print(roll,type(roll))
+marks=float(input("enter marks"))
+print(marks,type(marks))
+grade=input("enter grade")
+print(grade,type(grade))
